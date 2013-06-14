b87fd6af755413105375b94c097c6dc36db12cb9
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2013-06-14 21:50:10 +0300

    Renaming SoundTools and Tests project accordingly (camel case standard)

--- src/Soundfingerprinting.UnitTests/Properties/AssemblyInfo.cs
+++ src/Soundfingerprinting.UnitTests/Properties/AssemblyInfo.cs
@@ -1,42 +1,15 @@
-﻿// Sound Fingerprinting framework
-// git://github.com/AddictedCS/soundfingerprinting.git
-// Code license: CPOL v.1.02
-// ciumac.sergiu@gmail.com
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("SoundFingerprintingLib.Tests")]
+[assembly: AssemblyTitle("SoundFingerprinting.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("SoundFingerprintingLib.Tests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Ciumac Sergiu")]
+[assembly: AssemblyProduct("SoundFingerprinting.Tests")]
+[assembly: AssemblyCopyright("Copyright © Ciumac Sergiu 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("4cac962e-ebc5-4006-a1e0-7ffb3e2483c2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      BuildWorkUnit Number
-//      Revision
-//
-// You can specify all the values or you can default the BuildWorkUnit and Revision Numbers 
-// by using the '*' as shown below:
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
