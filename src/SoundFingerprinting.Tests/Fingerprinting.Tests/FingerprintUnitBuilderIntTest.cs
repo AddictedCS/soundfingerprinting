46a627f55b8ad4be88bfceb22c9e6ef8ebe37b1e
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2013-06-29 22:31:42 +0300

    Renaming test. Adjusting fingerprint unit builder test.
    Done in order to get rid of DirectSound library.

--- src/SoundFingerprinting.Tests/Fingerprinting.Tests/FingerprintUnitBuilderIntTest.cs
+++ src/SoundFingerprinting.Tests/Fingerprinting.Tests/FingerprintUnitBuilderIntTest.cs
@@ -10,11 +10,10 @@
     using Moq;
 
     using SoundFingerprinting.Audio.Bass;
-    using SoundFingerprinting.Audio.DirectSound;
+    using SoundFingerprinting.Audio.NAudio;
     using SoundFingerprinting.Configuration;
     using SoundFingerprinting.Dao;
     using SoundFingerprinting.Dao.Entities;
-    using SoundFingerprinting.DbStorage.Utils;
     using SoundFingerprinting.FFT;
     using SoundFingerprinting.FFT.FFTW;
     using SoundFingerprinting.Hashing.MinHash;
@@ -23,12 +22,12 @@
     using SoundFingerprinting.Wavelets;
 
     [TestClass]
-    public class FingerprintManagerTest : BaseTest
+    public class FingerprintUnitBuilderIntTest : BaseTest
     {
         private ModelService modelService;
         private IFingerprintService fingerprintService;
         private IFingerprintUnitBuilder fingerprintUnitBuilderWithBass;
-        private IFingerprintUnitBuilder fingerprintUnitBuilderWithDirectSound;
+        private IFingerprintUnitBuilder fingerprintUnitBuilderWithNAudio;
         private IFingerprintingConfiguration defaultConfiguration;
         private IPermutations permutations;
 
@@ -38,13 +37,9 @@
             modelService = new ModelService(new MsSqlDatabaseProviderFactory(new DefaultConnectionStringFactory()), new ModelBinderFactory());
             fingerprintService = new FingerprintService(new FingerprintDescriptor(), new SpectrumService(new CachedFFTWService(new FFTWService86())), new WaveletService(new StandardHaarWaveletDecomposition()));
             defaultConfiguration = new DefaultFingerprintingConfiguration();
-            var mockedPermutations = new Mock<IPermutations>();
-            mockedPermutations.Setup(perms => perms.GetPermutations()).Returns(new int[1][]);
-            permutations = mockedPermutations.Object;
+            permutations = new DefaultPermutations();
             fingerprintUnitBuilderWithBass = new FingerprintUnitBuilder(fingerprintService, new BassAudioService(), new MinHashService(permutations));
-#pragma warning disable 612,618
-            fingerprintUnitBuilderWithDirectSound = new FingerprintUnitBuilder(fingerprintService, new DirectSoundAudioService(), new MinHashService(permutations));
-#pragma warning restore 612,618
+            fingerprintUnitBuilderWithNAudio = new FingerprintUnitBuilder(fingerprintService, new NAudioService(), new MinHashService(permutations));
         }
 
         [TestCleanup]
@@ -61,8 +56,8 @@
         public void CreateFingerprintsFromFileAndInsertInDatabaseUsingDirectSoundProxyTest()
         {
             var track = InsertTrack();
-            var signatures = fingerprintUnitBuilderWithDirectSound.BuildFingerprints()
-                                            .On(PathToWav)
+            var signatures = fingerprintUnitBuilderWithNAudio.BuildFingerprints()
+                                            .On(PathToMp3)
                                             .With(defaultConfiguration)
                                             .RunAlgorithm()
                                             .Result;
@@ -94,8 +89,8 @@
         [TestMethod]
         public void CompareFingerprintsCreatedByDifferentProxiesTest()
         {
-            var directSoundFingerprints = fingerprintUnitBuilderWithDirectSound.BuildFingerprints()
-                                                        .On(PathToWav)
+            var naudioFingerprints = fingerprintUnitBuilderWithNAudio.BuildFingerprints()
+                                                        .On(PathToMp3)
                                                         .With(defaultConfiguration)
                                                         .RunAlgorithm()
                                                         .Result;
@@ -108,17 +103,18 @@
             int unmatchedItems = 0;
             int totalmatches = 0;
 
+            Assert.AreEqual(bassFingerprints.Count, naudioFingerprints.Count);
             for (
                 int i = 0,
-                    n = directSoundFingerprints.Count > bassFingerprints.Count
+                    n = naudioFingerprints.Count > bassFingerprints.Count
                             ? bassFingerprints.Count
-                            : directSoundFingerprints.Count;
+                            : naudioFingerprints.Count;
                 i < n;
                 i++)
             {
-                for (int j = 0; j < directSoundFingerprints[i].Length; j++)
+                for (int j = 0; j < naudioFingerprints[i].Length; j++)
                 {
-                    if (directSoundFingerprints[i][j] != bassFingerprints[i][j])
+                    if (naudioFingerprints[i][j] != bassFingerprints[i][j])
                     {
                         unmatchedItems++;
                     }
@@ -128,7 +124,7 @@
             }
 
             Assert.AreEqual(true, (float)unmatchedItems / totalmatches < 0.02);
-            Assert.AreEqual(bassFingerprints.Count, directSoundFingerprints.Count);
+            Assert.AreEqual(bassFingerprints.Count, naudioFingerprints.Count);
         }
 
         [TestMethod]
@@ -148,32 +144,6 @@
                 long expected = fileSize / (8192 * 4); // One fingerprint corresponds to a granularity of 8192 samples which is 16384 bytes
                 Assert.AreEqual(expected, list.Count);
                 File.Delete(tempFile);
-            }
-        }
-
-        [TestMethod]
-        public void GetDoubleArrayFromByteTest()
-        {
-            byte[] byteArray = TestUtilities.GenerateRandomInputByteArray(128 * 64);
-            bool silence = false;
-            float[] array = ArrayUtils.GetDoubleArrayFromSamples(byteArray, 128 * 64, ref silence);
-            for (int i = 0; i < array.Length; i++)
-            {
-                switch (byteArray[i])
-                {
-                    case 255:
-                        Assert.AreEqual(-1, array[i]);
-                        break;
-                    case 0:
-                        Assert.AreEqual(0, array[i]);
-                        break;
-                    case 1:
-                        Assert.AreEqual(1, array[i]);
-                        break;
-                    default:
-                        Assert.Fail("Wrong input");
-                        break;
-                }
             }
         }
 
