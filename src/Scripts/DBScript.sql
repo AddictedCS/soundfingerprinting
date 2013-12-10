ff8631978b12342ec65f2d9bf85eaf53bcd122db
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2013-12-10 19:59:36 +0200

    Cascading delete in case if DeleteTrack is invoked (deleting from HashTables and SubFingerprints).

--- src/Scripts/DBScript.sql
+++ src/Scripts/DBScript.sql
@@ -56,331 +56,180 @@ CREATE TABLE SubFingerprints
 	CONSTRAINT FK_SubFingerprints_Tracks FOREIGN KEY (TrackId) REFERENCES dbo.Tracks(Id)
 )
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_1')
-BEGIN
-	CREATE TABLE HashTable_1
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_1 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
-go
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_2')
-BEGIN
-	CREATE TABLE HashTable_2
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_2 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_1
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_1 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_3')
-BEGIN
-	CREATE TABLE HashTable_3
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_3 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_2
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_2 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_4')
-BEGIN
-	CREATE TABLE HashTable_4
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_4 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_3
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_3 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_5')
-BEGIN
-	CREATE TABLE HashTable_5
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_5 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_4
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_4 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_6')
-BEGIN
-	CREATE TABLE HashTable_6
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_6 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_5
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_5 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_7')
-BEGIN
-	CREATE TABLE HashTable_7
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_7 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_6
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_6 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_8')
-BEGIN
-	CREATE TABLE HashTable_8
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_8 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_7
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_7 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_9')
-BEGIN
-	CREATE TABLE HashTable_9
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_9 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_8
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_8 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_10')
-BEGIN
-	CREATE TABLE HashTable_10
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_10 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_9
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_9 PRIMARY KEY(HashBin, SubFingerprintId)	
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_11')
-BEGIN
-	CREATE TABLE HashTable_11
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_11 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_10
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_10 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_12')
-BEGIN
-	CREATE TABLE HashTable_12
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_12 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_11
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_11 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_13')
-BEGIN
-	CREATE TABLE HashTable_13
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_13 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_12
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_12 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_14')
-BEGIN
-	CREATE TABLE HashTable_14
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_14 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_13
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_13 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_15')
-BEGIN
-	CREATE TABLE HashTable_15
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_15 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_14
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_14 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_16')
-BEGIN
-	CREATE TABLE HashTable_16
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_16 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_15
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_15 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_17')
-BEGIN
-	CREATE TABLE HashTable_17
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_17 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_16
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_16 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_18')
-BEGIN
-	CREATE TABLE HashTable_18
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_18 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_17
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_17 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_19')
-BEGIN
-	CREATE TABLE HashTable_19
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_19 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_18
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_18 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_20')
-BEGIN
-	CREATE TABLE HashTable_20
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_20 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_19
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_19 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_21')
-BEGIN
-	CREATE TABLE HashTable_21
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_21 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_20
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_20 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_22')
-BEGIN
-	CREATE TABLE HashTable_22
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_22 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_21
+(	
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_21 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_23')
-BEGIN
-	CREATE TABLE HashTable_23
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_23 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_22
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_22 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_24')
-BEGIN
-	CREATE TABLE HashTable_24
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_24 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
-
+CREATE TABLE HashTable_23
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_23 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 GO
-IF NOT EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'HashTable_25')
-BEGIN
-	CREATE TABLE HashTable_25
-	(
-		HashBin BIGINT NOT NULL,								    
-		SubFingerprintId BIGINT NOT NULL,
-		CONSTRAINT PK_HashBinsMinHashId_25 PRIMARY KEY(HashBin, SubFingerprintId)
-	)
-END
+CREATE TABLE HashTable_24
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_24 PRIMARY KEY(HashBin, SubFingerprintId)
+)
+GO
+CREATE TABLE HashTable_25
+(
+	HashBin BIGINT NOT NULL,								    
+	SubFingerprintId BIGINT NOT NULL,
+	CONSTRAINT PK_HashBinsMinHashId_25 PRIMARY KEY(HashBin, SubFingerprintId)
+)
 
 GO
 -- TABLE WHICH WILL CONTAIN ALL THE INFORMATION RELATED TO THE PERMUTATIONS
@@ -717,9 +566,34 @@ CREATE PROCEDURE sp_DeleteTrack
 	@Id INT
 AS
 BEGIN
-	DELETE FROM Fingerprints WHERE Fingerprints.TrackId = @Id		-- CASCADE DELETE
-	DELETE FROM SubFingerprints WHERE SubFingerprints.TrackId = @Id -- CASCADE DELETE
-	DELETE FROM Tracks WHERE Tracks.Id = @Id						-- CASCADE DELETE
+	DELETE FROM Fingerprints WHERE Fingerprints.TrackId = @Id
+	DELETE HashTable_1 FROM HashTable_1 INNER JOIN SubFingerprints ON HashTable_1.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_2 FROM HashTable_2 INNER JOIN SubFingerprints ON HashTable_2.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_3 FROM HashTable_3 INNER JOIN SubFingerprints ON HashTable_3.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_4 FROM HashTable_4 INNER JOIN SubFingerprints ON HashTable_4.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_5 FROM HashTable_5 INNER JOIN SubFingerprints ON HashTable_5.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_6 FROM HashTable_6 INNER JOIN SubFingerprints ON HashTable_6.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_7 FROM HashTable_7 INNER JOIN SubFingerprints ON HashTable_7.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_8 FROM HashTable_8 INNER JOIN SubFingerprints ON HashTable_8.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_9 FROM HashTable_9 INNER JOIN SubFingerprints ON HashTable_9.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_10 FROM HashTable_10 INNER JOIN SubFingerprints ON HashTable_10.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_11 FROM HashTable_11 INNER JOIN SubFingerprints ON HashTable_11.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_12 FROM HashTable_12 INNER JOIN SubFingerprints ON HashTable_12.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_13 FROM HashTable_13 INNER JOIN SubFingerprints ON HashTable_13.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_14 FROM HashTable_14 INNER JOIN SubFingerprints ON HashTable_14.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_15 FROM HashTable_15 INNER JOIN SubFingerprints ON HashTable_15.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_16 FROM HashTable_16 INNER JOIN SubFingerprints ON HashTable_16.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_17 FROM HashTable_17 INNER JOIN SubFingerprints ON HashTable_17.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_18 FROM HashTable_18 INNER JOIN SubFingerprints ON HashTable_18.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_19 FROM HashTable_19 INNER JOIN SubFingerprints ON HashTable_19.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_20 FROM HashTable_20 INNER JOIN SubFingerprints ON HashTable_20.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_21 FROM HashTable_21 INNER JOIN SubFingerprints ON HashTable_21.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_22 FROM HashTable_22 INNER JOIN SubFingerprints ON HashTable_22.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_23 FROM HashTable_23 INNER JOIN SubFingerprints ON HashTable_23.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_24 FROM HashTable_24 INNER JOIN SubFingerprints ON HashTable_24.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE HashTable_25 FROM HashTable_25 INNER JOIN SubFingerprints ON HashTable_25.SubFingerprintId = SubFingerprints.Id AND SubFingerprints.TrackId = @Id
+	DELETE FROM SubFingerprints WHERE SubFingerprints.TrackId = @Id
+	DELETE FROM Tracks WHERE Tracks.Id = @Id
 END
 GO
 
