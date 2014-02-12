63acad5760e07c9f4aa53aa599c2248d162b866e
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2014-02-12 19:50:41 +0200

    Adding MongoDb project for NoSql backend. Adding tests.

---
+++ src/SoundFingerprinting.Dao.MongoDb/MongoDbModelService.cs
@@ -0,0 +1,69 @@
+﻿namespace SoundFingerprinting.Dao.MongoDb
+{
+    using System.Collections.Generic;
+
+    using SoundFingerprinting.Data;
+
+    public class MongoDbModelService : IModelService
+    {
+        public IModelReference InsertFingerprint(FingerprintData fingerprintData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IModelReference InsertTrack(TrackData track)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void InsertHashDataForTrack(IEnumerable<HashData> hashes, IModelReference trackReference)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<HashData> ReadHashDataByTrack(IModelReference trackReference)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<TrackData> ReadAllTracks()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<TrackData> ReadTrackByArtistAndTitleName(string artist, string title)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<FingerprintData> ReadFingerprintsByTrackReference(IModelReference trackReference)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TrackData ReadTrackByReference(IModelReference trackReference)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TrackData ReadTrackByISRC(string isrc)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int DeleteTrack(IModelReference trackReference)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<SubFingerprintData> ReadSubFingerprintDataByHashBucketsWithThreshold(long[] buckets, int threshold)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<SubFingerprintData> ReadSubFingerprintDataByHashBucketsThresholdWithGroupId(long[] buckets, int threshold, string trackGroupId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/SoundFingerprinting.Tests/Integration/Dao/MongoDb/MongoDbModelServiceTest.cs
@@ -0,0 +1,15 @@
+﻿namespace SoundFingerprinting.Tests.Integration.Dao.MongoDb
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using SoundFingerprinting.Dao;
+
+    [TestClass]
+    public class MongoDbModelServiceTest : ModelServiceTest
+    {
+        public MongoDbModelServiceTest(IModelService modelService)
+            : base(modelService)
+        {
+        }
+    }
+}
