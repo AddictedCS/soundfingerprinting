b8622fd11afa6c5487281a592139f8a18738bef4
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2014-03-12 10:27:51 +0200

    Testing continuous stream samples provider class.

---
+++ src/SoundFingerprinting.Tests/Unit/Audio/ContinuousStreamSamplesProviderTest.cs
@@ -0,0 +1,30 @@
+﻿namespace SoundFingerprinting.Tests.Unit.Audio
+{
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using SoundFingerprinting.Audio;
+
+    [TestClass]
+    public class ContinuousStreamSamplesProviderTest
+    {
+        private ContinuousStreamSamplesProvider samplesProvider;
+
+        [TestMethod]
+        public void TestGetNextSamples()
+        {
+            float[] buffer = new float[1024];
+            var queue = new Queue<int>(new[] { 1024, 0, 0, 512, 0, 0, 0, 256 });
+
+            samplesProvider = new ContinuousStreamSamplesProvider(new QueueSamplesProvider(queue));
+
+            int[] expectedResults = new[] { 1024, 512, 256 };
+            for (int i = 0; i < 3; i++)
+            {
+                var result = samplesProvider.GetNextSamples(buffer);
+                Assert.AreEqual(expectedResults[i], result);
+            }
+        }
+    }
+}
--- src/SoundFingerprinting/Audio/IPlayAudioFileService.cs
+++ src/SoundFingerprinting/Audio/IPlayAudioFileService.cs
@@ -2,17 +2,8 @@
 {
     public interface IPlayAudioFileService
     {
-        /// <summary>
-        ///   Play file
-        /// </summary>
-        /// <param name = "pathToFile">Filename</param>
-        /// <returns>Identifier for currently playing stream</returns>
         object PlayFile(string pathToFile);
 
-        /// <summary>
-        /// Stop playing stream
-        /// </summary>
-        /// <param name="stream">Identifier of the stream to be stopped</param>
         void StopPlayingFile(object stream);
     }
 }
