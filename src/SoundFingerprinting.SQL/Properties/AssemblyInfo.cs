2a7afb773070842de7d971d119beb55ea4390241
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2014-03-16 20:02:28 +0200

    Adding SQL project for MSSQL backend implementation.

---
+++ src/SoundFingerprinting.SQL/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SoundFingerprinting.SQL")]
+[assembly: AssemblyDescription("SQL backend support for SoundFingerprinting framework.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ciumac Sergiu")]
+[assembly: AssemblyProduct("SoundFingerprinting.SQL")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("134d1a0d-53d7-40a4-8a47-3353d2a08985")]
+[assembly: AssemblyVersion("0.1.0.*")]
