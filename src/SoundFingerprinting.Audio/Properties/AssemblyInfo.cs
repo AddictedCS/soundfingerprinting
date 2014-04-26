3ca1c6d37f51fa207f08835b52e9f0c9cda1efc3
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2014-04-26 19:29:21 +0300

    Adding details to Audio and Infrastructure libraries.

--- src/SoundFingerprinting.Audio/Properties/AssemblyInfo.cs
+++ src/SoundFingerprinting.Audio/Properties/AssemblyInfo.cs
@@ -3,19 +3,17 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("SoundFingerprinting.Audio")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Audio interfaces for SoundFingerprinting library.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SoundFingerprinting.Audio")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Ciumac Sergiu 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("58094676-8f2b-42b6-8db5-a7aaddc00c98")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.*")]
 [assembly: InternalsVisibleTo("SoundFingerprinting")]
 [assembly: InternalsVisibleTo("SoundFingerprinting.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-
-
