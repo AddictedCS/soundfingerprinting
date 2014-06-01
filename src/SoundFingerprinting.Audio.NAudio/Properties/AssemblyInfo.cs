52bee70745a95a11ee15da84a0f81760c0e7e47b
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2014-06-01 14:14:04 +0300

    Getting rid of auto-increment as it is hard to properly maintain versioning. Incremental manual releases will follow.

--- src/SoundFingerprinting.Audio.NAudio/Properties/AssemblyInfo.cs
+++ src/SoundFingerprinting.Audio.NAudio/Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8750fb98-a464-4917-8489-19cc756b4d68")]
-[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyVersion("1.0.2.0")]
 [assembly: InternalsVisibleTo("SoundFingerprinting.Audio.NAudio.Test")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
