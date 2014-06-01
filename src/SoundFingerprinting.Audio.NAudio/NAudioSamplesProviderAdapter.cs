e77b155dcb8ce0bb2d0e013de9704eb7a5253bdc
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2014-06-01 17:38:05 +0300

    Renaming NAudioSamplesProvider to NAudioSamplesProviderAdapter.

---
+++ src/SoundFingerprinting.Audio.NAudio/NAudioSamplesProviderAdapter.cs
@@ -0,0 +1,19 @@
+﻿namespace SoundFingerprinting.Audio.NAudio
+{
+    using global::NAudio.Wave;
+
+    public class NAudioSamplesProviderAdapter : ISamplesProvider
+    {
+        private readonly ISampleProvider samplesProvider;
+
+        public NAudioSamplesProviderAdapter(ISampleProvider samplesProvider)
+        {
+            this.samplesProvider = samplesProvider;
+        }
+
+        public int GetNextSamples(float[] buffer)
+        {
+            return samplesProvider.Read(buffer, 0, buffer.Length) * 4;
+        }
+    }
+}
