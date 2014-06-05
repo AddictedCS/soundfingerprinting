e027dd8bc53aceb45e1c5ac26bcbe57602d28f20
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2014-06-05 20:52:27 +0300

    Testing public interfaces are resolved by dependency resolver.

---
+++ src/SoundFingerprinting.Audio.NAudio.Test/DependencyResolverTest.cs
@@ -0,0 +1,20 @@
+﻿namespace SoundFingerprinting.Audio.NAudio.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using SoundFingerprinting.Audio.NAudio.Play;
+
+    [TestClass]
+    public class DependencyResolverTest
+    {
+        [TestMethod]
+        public void TestPublicInterfacesAreResolvedFromNAudioModule()
+        {
+            var naudioService = new NAudioService();
+            var naudioPlayService = new NAudioPlayAudioFileService();
+
+            Assert.IsNotNull(naudioPlayService);
+            Assert.IsNotNull(naudioService);
+        }
+    }
+}
