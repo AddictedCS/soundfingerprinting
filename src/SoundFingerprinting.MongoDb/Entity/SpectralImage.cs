1de665806fa80d6d658be768552f4b4bbafd9616
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2014-06-20 11:53:56 +0300

    Adding Insert/Get operation on ModelService class for spectral images handling.

---
+++ src/SoundFingerprinting.MongoDb/Entity/SpectralImage.cs
@@ -0,0 +1,15 @@
+﻿namespace SoundFingerprinting.MongoDb.Entity
+{
+    using MongoDB.Bson;
+
+    internal class SpectralImage
+    {
+        public ObjectId Id { get; set; }
+
+        public ObjectId TrackId { get; set; }
+
+        public float[] Image { get; set; }
+
+        public int OrderNumber { get; set; }
+    }
+}
