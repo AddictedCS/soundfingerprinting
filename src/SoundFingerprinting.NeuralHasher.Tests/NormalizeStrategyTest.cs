4f17986d27f5ed1e789a410fdf67ddcb5396d138
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2014-06-22 16:39:20 +0300

    Adding normalization strategy, which will normalize input/outputs according to activation function which is used internally.

---
+++ src/SoundFingerprinting.NeuralHasher.Tests/NormalizeStrategyTest.cs
@@ -0,0 +1,37 @@
+﻿namespace SoundFingerprinting.NeuralHasher.Tests
+{
+    using Encog.Engine.Network.Activation;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using SoundFingerprinting.NeuralHasher.Utils;
+    using SoundFingerprinting.Tests;
+
+    [TestClass]
+    public class NormalizeStrategyTest : AbstractTest
+    {
+        private readonly INormalizeStrategy normalizeStrategy = new NormalizeStrategy();
+
+        [TestMethod]
+        public void TestOutputIsNomalizedAccordingToTANHFunction()
+        {
+            var activationFunction = new ActivationTANH();
+            double[] output = new double[] { 1, 0, 1, 0 };
+
+            normalizeStrategy.NormalizeOutputInPlace(activationFunction, output);
+
+            AssertArraysAreEqual(new double[] { 1, -1, 1, -1 }, output);
+        }
+
+        [TestMethod]
+        public void TestOutputIsNormalizedAccordingToSigmoidFunction()
+        {
+            var activationFunction = new ActivationSigmoid();
+            double[] output = new double[] { 1, 0, 1, 0 };
+
+            normalizeStrategy.NormalizeOutputInPlace(activationFunction, output);
+
+            AssertArraysAreEqual(new double[] { 1, 0, 1, 0 }, output);
+        }
+    }
+}
---
+++ src/SoundFingerprinting.NeuralHasher/Utils/NormalizeStrategy.cs
@@ -0,0 +1,63 @@
+﻿namespace SoundFingerprinting.NeuralHasher.Utils
+{
+    using System;
+
+    using Encog.Engine.Network.Activation;
+
+    internal class NormalizeStrategy : INormalizeStrategy
+    {
+        private const double Epsilon = 0.001;
+        
+        public void NormalizeOutputInPlace(IActivationFunction function, double[] output)
+        {
+            if (function is ActivationTANH)
+            {
+                for (int i = 0, n = output.Length; i < n; i++)
+                {
+                    output[i] = output[i] > 0.5 ? 1 : -1;
+                }
+            }
+            else if (function is ActivationSigmoid)
+            {
+                /*do nothing*/
+            }
+            else
+            {
+                throw new ArgumentException("Unknown activation function");
+            }
+        }
+
+        public void NormalizeInputInPlace(IActivationFunction function, double[] input)
+        {
+            if (function is ActivationTANH)
+            {
+                for (int i = 0, n = input.Length; i < n; i++)
+                {
+                    input[i] = Math.Abs(input[i] - 0) < Epsilon ? 0.0f : (input[i] < 0 ? -0.8f : 0.8f);
+                }
+            }
+            else if (function is ActivationSigmoid)
+            {
+                for (int i = 0, n = input.Length; i < n; i++)
+                {
+                    input[i] = Math.Abs(input[i] - 0) < Epsilon ? 0.0f : (input[i] < 0 ? 0.2f : 0.8f);
+                }
+            }
+            else if (function is ActivationLinear)
+            {
+                /*do nothing*/
+            }
+            else
+            {
+                throw new ArgumentException("Unknown activation function");
+            }
+        }
+    }
+
+    internal interface INormalizeStrategy
+    {
+        void NormalizeOutputInPlace(IActivationFunction function, double[] output);
+
+        void NormalizeInputInPlace(IActivationFunction function, double[] input);
+    }
+}
