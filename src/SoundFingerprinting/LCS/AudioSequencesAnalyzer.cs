e6675398685cdd0474c8056079011b7874d1eb37
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2014-11-06 15:08:56 +0200

    Adding tests for sequence analyzer.

---
+++ src/SoundFingerprinting/LCS/AudioSequencesAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿namespace SoundFingerprinting.LCS
+{
+    using System.Collections.Generic;
+
+    using SoundFingerprinting.Data;
+
+    public class AudioSequencesAnalyzer : IAudioSequencesAnalyzer
+    {
+        public IEnumerable<SubFingerprintData> GetLongestIncreasingSubSequence(
+            IEnumerable<SubFingerprintData> sequence)
+        {
+            return null;
+        }
+    }
+
+    public interface IAudioSequencesAnalyzer
+    {
+        IEnumerable<SubFingerprintData> GetLongestIncreasingSubSequence(IEnumerable<SubFingerprintData> sequence);
+    }
+}
