b3943add4fce84544dd544e70fc5a176675012a1
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2015-03-07 12:54:53 +0200

    Upgrading version information. This release in not going to be backward compatible. Quite a few interfaces have changed. Lets release 2.0 version

--- src/SoundFingerprinting.Audio.Bass/Properties/AssemblyInfo.cs
+++ src/SoundFingerprinting.Audio.Bass/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("d8f2b348-be22-416e-b603-de7b58bbef6e")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: InternalsVisibleTo("SoundFingerprinting.Audio.Bass.Tests")]
-[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyVersion("2.0.0")]
