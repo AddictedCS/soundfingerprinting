43b14f21b80df920b9aa2f10c83b813993919c0b
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2015-05-02 13:17:37 +0300

    SF-47 Bass has its own repository now.

--- src/SoundFingerprinting.Audio.Bass/BassConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SoundFingerprinting.Audio.Bass
-{
-    internal static class BassConstants
-    {
-        public const int NumberOfChannels = 1;
-    }
-}
--- src/SoundFingerprinting.Audio.Bass/IBassResampler.cs
+++
@@ -1,9 +0,0 @@
-namespace SoundFingerprinting.Audio.Bass
-{
-    using System;
-
-    internal interface IBassResampler
-    {
-        float[] Resample(int sourceStream, int sampleRate, int seconds, int startAt, Func<int, ISamplesProvider> getSamplesProvider);
-    }
-}
