a9484ac840392c7a510662b56dcf62c2e4c5acd8
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2015-05-16 11:48:22 +0300

    Adding more details on the implementation.

--- src/SoundFingerprinting/Command/IQueryCommand.cs
+++ src/SoundFingerprinting/Command/IQueryCommand.cs
@@ -7,12 +7,26 @@
 
     public interface IQueryCommand
     {
+        /// <summary>
+        /// Gets fingerprint configuration used to create fingerprints during query snippet processing.
+        /// </summary>
         FingerprintConfiguration FingerprintConfiguration { get; }
 
+        /// <summary>
+        /// Gets query configuration used to query the underlying data source with fingerprints created from the query snippet
+        /// </summary>
         QueryConfiguration QueryConfiguration { get; }
 
+        /// <summary>
+        ///  Query the underlying data source capturing best candidate and similarity information
+        /// </summary>
+        /// <returns>Query result</returns>
         Task<QueryResult> Query();
         
+        /// <summary>
+        ///  Query the underlying data source capturing best candidate as well as time location within the track
+        /// </summary>
+        /// <returns>Query result</returns>
         Task<QueryResult> QueryWithTimeSequenceInformation();
     }
 }
