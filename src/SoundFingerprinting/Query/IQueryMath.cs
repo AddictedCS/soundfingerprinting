55b3caa052913c5344ec07bf9ea34440399d9bd9
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2016-11-09 17:44:18 +0200

    Adding a method to filter out best candidates from the list of hamming distances.

--- src/SoundFingerprinting/Query/IQueryMath.cs
+++ src/SoundFingerprinting/Query/IQueryMath.cs
@@ -1,9 +1,15 @@
 namespace SoundFingerprinting.Query
 {
+    using System.Collections.Generic;
+
     using SoundFingerprinting.Configuration;
+    using SoundFingerprinting.DAO;
+    using SoundFingerprinting.Data;
 
     internal interface IQueryMath
     {
-        double AdjustSnippetLengthToConfigsUsedDuringFingerprinting(double snipetLength, FingerprintConfiguration fingerprintConfiguration);
+        double CalculateExactSnippetLength(IEnumerable<HashedFingerprint> hashedFingerprints, FingerprintConfiguration fingerprintConfiguration);
+
+        List<ResultEntry> GetBestCandidates(IDictionary<IModelReference, int> hammingSimilarites, int numberOfCandidatesToReturn, IModelService modelService);
     }
 }
--- src/SoundFingerprinting/Query/QueryMath.cs
+++ src/SoundFingerprinting/Query/QueryMath.cs
@@ -1,10 +1,38 @@
 ﻿namespace SoundFingerprinting.Query
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     using SoundFingerprinting.Configuration;
+    using SoundFingerprinting.DAO;
+    using SoundFingerprinting.Data;
 
     internal class QueryMath : IQueryMath
     {
-        public double AdjustSnippetLengthToConfigsUsedDuringFingerprinting(double snipetLength, FingerprintConfiguration fingerprintConfiguration)
+        public double CalculateExactSnippetLength(IEnumerable<HashedFingerprint> hashedFingerprints, FingerprintConfiguration fingerprintConfiguration)
+        {
+            double min = double.MaxValue, max = double.MinValue;
+            foreach (var hashedFingerprint in hashedFingerprints)
+            {
+                min = System.Math.Min(min, hashedFingerprint.Timestamp);
+                max = System.Math.Max(max, hashedFingerprint.Timestamp);
+            }
+
+            return AdjustSnippetLengthToConfigsUsedDuringFingerprinting(max - min, fingerprintConfiguration);
+        }
+
+        public List<ResultEntry> GetBestCandidates(IDictionary<IModelReference, int> hammingSimilarites, int numberOfCandidatesToReturn, IModelService modelService)
+        {
+            return hammingSimilarites.OrderByDescending(e => e.Value)
+                                     .Take(numberOfCandidatesToReturn)
+                                     .Select(e => new ResultEntry
+                                         {
+                                             Track = modelService.ReadTrackByReference(e.Key), MatchedFingerprints = e.Value
+                                         })
+                                    .ToList();
+        }
+
+        private double AdjustSnippetLengthToConfigsUsedDuringFingerprinting(double snipetLength, FingerprintConfiguration fingerprintConfiguration)
         {
             int sampleRate = fingerprintConfiguration.SampleRate;
             int wdftSize = fingerprintConfiguration.SpectrogramConfig.WdftSize;
