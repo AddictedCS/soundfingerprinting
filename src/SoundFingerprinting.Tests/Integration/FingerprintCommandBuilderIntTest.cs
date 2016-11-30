98f7cd1ea94146decc34d9a23e60fda695b6c85a
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2016-11-30 16:35:57 +0200

    Enhacing command builder tests

--- src/SoundFingerprinting.Tests/Integration/FingerprintCommandBuilderIntTest.cs
+++ src/SoundFingerprinting.Tests/Integration/FingerprintCommandBuilderIntTest.cs
@@ -21,22 +21,12 @@
     {
         private readonly DefaultFingerprintConfiguration config = new DefaultFingerprintConfiguration();
 
-        private readonly IModelService modelService;
-        private readonly IFingerprintCommandBuilder fingerprintCommandBuilder;
-        private readonly IQueryFingerprintService queryFingerprintService;
-        private readonly IAudioService audioService;
-        private readonly ITagService tagService;
-        private readonly IWaveFileUtility waveUtility;
-
-        public FingerprintCommandBuilderIntTest()
-        {
-            audioService = new NAudioService();
-            tagService = new NAudioTagService();
-            waveUtility = new NAudioWaveFileUtility();
-            modelService = new InMemoryModelService();
-            fingerprintCommandBuilder = new FingerprintCommandBuilder();
-            queryFingerprintService = new QueryFingerprintService();
-        }
+        private readonly IModelService modelService = new InMemoryModelService();
+        private readonly IFingerprintCommandBuilder fingerprintCommandBuilder = new FingerprintCommandBuilder();
+        private readonly IQueryCommandBuilder queryCommandBuilder = new QueryCommandBuilder();
+        private readonly IAudioService audioService = new NAudioService();
+        private readonly ITagService tagService = new NAudioTagService();
+        private readonly IWaveFileUtility waveUtility = new NAudioWaveFileUtility();
 
         [TearDown]
         public void TearDown()
@@ -69,28 +59,35 @@
         }
 
         [Test]
-        public void CreateFingerprintsInsertThenQueryAndGetTheRightResult()
+        public void ShouldCreateFingerprintsInsertThenQueryAndGetTheRightResult()
         {
             const int SecondsToProcess = 10;
             const int StartAtSecond = 30;
-            var info = tagService.GetTagInfo(PathToMp3);
-            var track = new TrackData(info.ISRC, info.Artist, info.Title, info.Album, info.Year, info.Duration);
+            var tags = tagService.GetTagInfo(PathToMp3);
+            var track = new TrackData(tags);
             var trackReference = modelService.InsertTrack(track);
 
             var hashDatas = fingerprintCommandBuilder
                                             .BuildFingerprintCommand()
-                                            .From(PathToMp3, SecondsToProcess, StartAtSecond)
+                                            .From(PathToMp3)
                                             .UsingServices(audioService)
                                             .Hash()
                                             .Result;
 
             modelService.InsertHashDataForTrack(hashDatas, trackReference);
 
-            var queryResult = queryFingerprintService.Query(hashDatas, new DefaultQueryConfiguration(), modelService);
+            var queryResult = queryCommandBuilder.BuildQueryCommand()
+                               .From(PathToMp3, SecondsToProcess, StartAtSecond)
+                               .UsingServices(modelService, audioService)
+                               .Query()
+                               .Result;
 
             Assert.IsTrue(queryResult.ContainsMatches);
             Assert.AreEqual(1, queryResult.ResultEntries.Count());
             Assert.AreEqual(trackReference, queryResult.BestMatch.Track.TrackReference);
+            Assert.IsTrue(queryResult.BestMatch.QueryMatchLength > SecondsToProcess - 2);
+            Assert.AreEqual(StartAtSecond, Math.Abs(queryResult.BestMatch.TrackStartsAt), 0.1d);
+            Assert.IsTrue(queryResult.BestMatch.Confidence > 0.8);
         }
 
         [Test]
