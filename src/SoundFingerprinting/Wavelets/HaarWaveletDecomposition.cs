d6c200d1927ce7e441f7e8f42c835b0a1754be9c
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2016-11-30 02:59:32 -0800

    Non-standard wavelet decomposition is not really needed. It was used for experiments, now we can get rid of it.

--- src/SoundFingerprinting/Wavelets/HaarWaveletDecomposition.cs
+++ src/SoundFingerprinting/Wavelets/HaarWaveletDecomposition.cs
@@ -18,10 +18,11 @@
             float[] temp = new float[h];
 
             h /= 2;
+            var sqrt = Math.Sqrt(2.0);
             for (int i = 0; i < h; i++)
             {
-                temp[i] = (float)((array[2 * i] + array[(2 * i) + 1]) / Math.Sqrt(2.0));
-                temp[i + h] = (float)((array[2 * i] - array[(2 * i) + 1]) / Math.Sqrt(2.0));
+                temp[i] = (float)((array[2 * i] + array[(2 * i) + 1]) / sqrt);
+                temp[i + h] = (float)((array[2 * i] - array[(2 * i) + 1]) / sqrt);
             }
 
             for (int i = 0; i < (h * 2); i++)
