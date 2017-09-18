32df813e7ea9e7e45c80ee7cfdfbdedd30ae776f
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2017-09-18 11:33:31 +0300

    Adding min hash algorithm tests which uses Monte Carlo simulations to validate that theoretical data matches practical matches for MinHash and Jaccard Similarity.

--- src/SoundFingerprinting/MinHash/MinHashService.cs
+++ src/SoundFingerprinting/MinHash/MinHashService.cs
@@ -1,5 +1,7 @@
 ﻿namespace SoundFingerprinting.MinHash
 {
+    using System;
+
     using SoundFingerprinting.Infrastructure;
 
     internal class MinHashService : IMinHashService
@@ -24,26 +26,39 @@
             }
         }
 
-        public byte[] Hash(bool[] fingerprint)
+        public byte[] Hash(bool[] fingerprint, int n)
         {
-            return ComputeMinHashSignature(fingerprint);
+            return ComputeMinHashSignature(fingerprint, n);
         }
 
         /// <summary>
-        ///   Compute Min Hash signature of a fingerprint
+        /// Compute Min Hash signature of a fingerprint
         /// </summary>
-        /// <param name = "fingerprint">Fingerprint signature</param>
-        /// <returns>P-sized sub-fingerprint (length of the permutations number)</returns>
+        /// <param name="fingerprint">
+        /// Fingerprint signature
+        /// </param>
+        /// <param name="n">
+        /// The number of hash functions to use
+        /// </param>
+        /// <returns>
+        /// N-sized sub-fingerprint (length of the permutations number)
+        /// </returns>
         /// <remarks>
-        ///   The basic idea in the Min Hashing scheme is to randomly permute the rows and for each 
-        ///   column c(i) compute its hash value h(c(i)) as the index of the first row under the permutation that has a 1 in that column.
+        /// The basic idea in the Min Hashing scheme is to randomly permute the rows and for each 
+        /// column c(i) compute its hash value h(c(i)) as the index of the first row under the permutation that has a 1 in that column.
+        /// I.e. http://infolab.stanford.edu/~ullman/mmds/book.pdf s.3.3.4
         /// </remarks>
-        private byte[] ComputeMinHashSignature(bool[] fingerprint)
+        private byte[] ComputeMinHashSignature(bool[] fingerprint, int n)
         {
+            if (n > PermutationsCount)
+            {
+                throw new ArgumentException("n should not exceed number of available hash functions: " + PermutationsCount);
+            }
+
             bool[] signature = fingerprint;
             int[][] perms = permutations.GetPermutations();
-            byte[] minHash = new byte[perms.Length]; /*100*/
-            for (int i = 0; i < perms.Length /*100*/; i++)
+            byte[] minHash = new byte[n]; /*100*/
+            for (int i = 0; i < n; i++)
             {
                 minHash[i] = 255; /*The probability of occurrence of 1 after position 255 is very insignificant*/
                 for (int j = 0; j < perms[i].Length /*256*/; j++)
