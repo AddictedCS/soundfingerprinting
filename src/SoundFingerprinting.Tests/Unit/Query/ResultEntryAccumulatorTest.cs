AddictedCS/soundfingerprinting

commit d23b171686f82d28e9bbba1483f73f9efff344bd

Improving speed of querying via parallel for, better Hamming distance calculation and proper dictionary handling.

src/SoundFingerprinting/InMemory/RAMStorage.cs
@@ -42,7 +42,7 @@
         [ProtoMember(4)]
         public IDictionary<int, TrackData> Tracks { get; private set; }
 
-        private ConcurrentDictionary<long, List<ulong>>[] HashTables { get; set; }
+        private Dictionary<long, List<ulong>>[] HashTables { get; set; }
 
         [ProtoMember(5)]
         private IDictionary<ulong, SubFingerprintData> SubFingerprints
@@ -132,14 +132,14 @@
             return count;
         }
 
-        public IEnumerable<ulong> GetSubFingerprintsByHashTableAndHash(int table, long hash)
+        public List<ulong> GetSubFingerprintsByHashTableAndHash(int table, long hash)
         {
             if (HashTables[table].TryGetValue(hash, out var subFingerprintIds))
             {
                 return subFingerprintIds;
             }
 
-            return Enumerable.Empty<ulong>();
+            return Enumerable.Empty<ulong>().ToList();
         }
 
         public SubFingerprintData ReadSubFingerprintById(ulong id)
@@ -196,10 +196,10 @@
         {
             if (HashTables == null)
             {
-                HashTables = new ConcurrentDictionary<long, List<ulong>>[numberOfHashTables];
+                HashTables = new Dictionary<long, List<ulong>>[numberOfHashTables];
                 for (int table = 0; table < numberOfHashTables; table++)
                 {
-                    HashTables[table] = new ConcurrentDictionary<long, List<ulong>>();
+                    HashTables[table] = new Dictionary<long, List<ulong>>();
                 }
             }
         }

build.cmd
@@ -7,4 +7,5 @@ set config=%2
 if "%config%" == "" (
    set config=Release
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build\Build.proj /t:"%target%" /p:Configuration="%config%" /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild" build\Build.proj /t:"%target%" /p:Configuration="%config%" /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false

src/SoundFingerprinting.Tests/Unit/Math/SimilarityUtilityTest.cs
@@ -18,6 +18,27 @@
     public class SimilarityUtilityTest : AbstractTest
     {
         private readonly ISimilarityUtility similarityUtility = new SimilarityUtility();
+        private readonly IHashConverter hashConverter = new HashConverter();
+
+        [Test]
+        public void ShouldCorrectlyCalculateHammingDistanceBetweenLongs()
+        {
+            var length = 50000;
+
+            for (int run = 0; run < 1000; run++)
+            {
+                var x = GenerateByteArray(length);
+                var a = hashConverter.ToLongs(x, length / 4);
+
+                var y = GenerateByteArray(length);
+                var b = hashConverter.ToLongs(y, length / 4);
+
+                var byteSimilarity = similarityUtility.CalculateHammingSimilarity(x, y);
+                var longSimilarity = similarityUtility.CalculateHammingSimilarity(a, b, 4);
+
+                Assert.AreEqual(byteSimilarity, longSimilarity);
+            }
+        }
 
         [Test]
         public void ShouldSumUpHammingDistanceAccrossTracks()
@@ -177,6 +198,15 @@
 
             subFingerprints.Add(new SubFingerprintData(GenericHashBuckets(), candidatesCount - 1, OneFingerprintLength * candidatesCount, new ModelReference<int>(candidatesCount), trackReference));
             return subFingerprints;
+        }
+
+        private byte[] GenerateByteArray(int length)
+        {
+            Random ran = new Random();
+            byte[] random = new byte[length];
+            ran.NextBytes(random);
+
+            return random;
         }
     }
 }

src/SoundFingerprinting/InMemory/IRAMStorage.cs
@@ -16,7 +16,7 @@ namespace SoundFingerprinting.InMemory
 
         void AddSubfingerprint(HashedFingerprint hashedFingerprint, IModelReference trackReference);
 
-        IEnumerable<ulong> GetSubFingerprintsByHashTableAndHash(int table, long hash);
+        List<ulong> GetSubFingerprintsByHashTableAndHash(int table, long hash);
 
         int NumberOfHashTables { get; }
 

src/SoundFingerprinting/InMemory/SubFingerprintDao.cs
@@ -85,11 +85,18 @@
 
         private Dictionary<ulong, int> CountSubFingerprintMatches(long[] hashes)
         {
-            var subFingeprintCount = new Dictionary<ulong, int>();
+            var results = new List<ulong>[hashes.Length];
             for (int table = 0; table < hashes.Length; ++table)
             {
                 var hashBin = hashes[table];
-                foreach (var subFingerprintId in storage.GetSubFingerprintsByHashTableAndHash(table, hashBin))
+                results[table] = storage.GetSubFingerprintsByHashTableAndHash(table, hashBin);
+            }
+
+            int length = results.Select(entry => entry.Count).Sum();
+            var subFingeprintCount = new Dictionary<ulong, int>(length);
+            for (int table = 0; table < hashes.Length; ++table)
+            {
+                foreach (var subFingerprintId in results[table])
                 {
                     IncrementSubFingerprintCount(subFingeprintCount, subFingerprintId);
                 }
@@ -100,12 +107,9 @@
 
         private void IncrementSubFingerprintCount(IDictionary<ulong, int> subFingeprintCount, ulong subFingerprintId)
         {
-            if (!subFingeprintCount.ContainsKey(subFingerprintId))
-            {
-                subFingeprintCount[subFingerprintId] = 0;
-            }
-
-            subFingeprintCount[subFingerprintId]++;
+            int count;
+            subFingeprintCount.TryGetValue(subFingerprintId, out count);
+            subFingeprintCount[subFingerprintId] = ++count;
         }
     }
 }

src/SoundFingerprinting/Math/ISimilarityUtility.cs
@@ -14,6 +14,8 @@ namespace SoundFingerprinting.Math
 
         int CalculateHammingSimilarity(byte[] a, byte[] b);
 
+        int CalculateHammingSimilarity(long[] expected, long[] actual, int setBytesPerLong);
+
         /// <summary>
         ///   Calculate similarity between 2 fingerprints.
         /// </summary>

src/SoundFingerprinting/Math/SimilarityUtility.cs
@@ -84,14 +84,44 @@
         {
             foreach (var subFingerprint in candidates)
             {
-                byte[] signature = hashConverter.ToBytes(subFingerprint.Hashes, expected.SubFingerprint.Length);
-                int hammingSimilarity = CalculateHammingSimilarity(expected.SubFingerprint, signature);
+                var expectedHashBins = expected.HashBins;
+                var actualHashBins = subFingerprint.Hashes;
+                int hammingSimilarity = CalculateHammingSimilarity(
+                    expectedHashBins,
+                    actualHashBins,
+                    expected.SubFingerprint.Length / expected.HashBins.Length); 
+
                 SubFingerprintData fingerprint = subFingerprint;
                 accumulator.AddOrUpdate(
                     subFingerprint.TrackReference,
                     reference => new ResultEntryAccumulator(expected, fingerprint, hammingSimilarity),
                     (reference, entryAccumulator) => entryAccumulator.Add(expected, fingerprint, hammingSimilarity));
             }
+        }
+
+        public int CalculateHammingSimilarity(long[] expected, long[] actual, int setBytesPerLong)
+        {
+            int mask = 0xFF;
+            int sameBytes = 0;
+
+            for (int i = 0; i < expected.Length; ++i)
+            {
+                long a = expected[i];
+                long b = actual[i];
+
+                for (int j = 0; j < setBytesPerLong; ++j)
+                {
+                    if ((a & mask) == (b & mask))
+                    {
+                        sameBytes++;
+                    }
+
+                    a = a >> 8;
+                    b = b >> 8;
+                }
+            }
+
+            return sameBytes;
         }
     }
 }

commit fbd9b4e324aee05278b59dc286b58c442e922ac9

Bringing back ConcurrentDictionary for HashTables. It seems that commit #d23b171 introduced issues that drastically lowered the recognition rate ~790 ads to ~710. Hunting that bug.

src/SoundFingerprinting/InMemory/RAMStorage.cs
@@ -42,7 +42,7 @@
         [ProtoMember(4)]
         public IDictionary<int, TrackData> Tracks { get; private set; }
 
-        private Dictionary<long, List<ulong>>[] HashTables { get; set; }
+        private ConcurrentDictionary<long, List<ulong>>[] HashTables { get; set; }
 
         [ProtoMember(5)]
         private IDictionary<ulong, SubFingerprintData> SubFingerprints
@@ -196,10 +196,10 @@
         {
             if (HashTables == null)
             {
-                HashTables = new Dictionary<long, List<ulong>>[numberOfHashTables];
+                HashTables = new ConcurrentDictionary<long, List<ulong>>[numberOfHashTables];
                 for (int table = 0; table < numberOfHashTables; table++)
                 {
-                    HashTables[table] = new Dictionary<long, List<ulong>>();
+                    HashTables[table] = new ConcurrentDictionary<long, List<ulong>>();
                 }
             }
         }

src/SoundFingerprinting/Properties/AssemblyInfo.cs
@@ -29,4 +29,4 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("SoundFingerprinting.DuplicatesDetector")]
 
 [assembly: AssemblyVersion("3.1.3")]
-[assembly: AssemblyInformationalVersion("3.1.3-alpha7")]
+[assembly: AssemblyInformationalVersion("3.1.3-alpha8")]

src/SoundFingerprinting.Tests/SoundFingerprinting.Tests.csproj
@@ -110,6 +110,7 @@
     <Compile Include="Unit\Query\ConfidenceCalculatorTest.cs" />
     <Compile Include="Unit\Query\MatchedPairTest.cs" />
     <Compile Include="Unit\Query\QueryMathTest.cs" />
+    <Compile Include="Unit\Query\ResultEntryAccumulatorTest.cs" />
     <Compile Include="Unit\Strides\IncrementalRandomStrideTest.cs" />
     <Compile Include="Unit\Utils\ArrayUtilsTest.cs" />
     <Compile Include="Unit\Utils\FastFingerprintDescriptorTest.cs" />

src/SoundFingerprinting.Tests/Unit/Query/ResultEntryAccumulatorTest.cs
@@ -1,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoundFingerprinting.Tests.Unit.Query
+{
+    using System.Collections.Concurrent;
+
+    using NUnit.Framework;
+
+    using SoundFingerprinting.Data;
+    using SoundFingerprinting.DAO;
+    using SoundFingerprinting.DAO.Data;
+    using SoundFingerprinting.Math;
+    using SoundFingerprinting.Query;
+
+    [TestFixture]
+    public class ResultEntryAccumulatorTest
+    {
+        private readonly ISimilarityUtility similarityUtility = new SimilarityUtility(new HashConverter());
+
+        [Test]
+        public void ShouldCalculateSimilaritiesCorrectly()
+        {
+            var hammingSimilarities = new ConcurrentDictionary<IModelReference, ResultEntryAccumulator>();
+            var totalCount = 10000;
+            var countPerTrack = 10;
+            var tracksCount = totalCount / countPerTrack;
+            var subFingerprints = GetSubFingerprints(totalCount, countPerTrack);
+            var queryFingerprint = new HashedFingerprint(new byte[100], new long[25], 0,0, Enumerable.Empty<string>());
+
+            Parallel.ForEach(subFingerprints, subFingerprint => { 
+                var toCompare = new List<SubFingerprintData>(new[] { subFingerprint });
+                similarityUtility.AccumulateHammingSimilarity(toCompare, queryFingerprint, hammingSimilarities);
+            });
+
+            Assert.AreEqual(tracksCount, hammingSimilarities.Count);
+            foreach (var pair in hammingSimilarities)
+            {
+                Assert.AreEqual(countPerTrack, pair.Value.Matches.Count);
+                Assert.AreEqual(countPerTrack * 100, pair.Value.HammingSimilaritySum);
+                Assert.AreEqual(100, pair.Value.BestMatch.HammingSimilarity);
+            }
+        }
+
+        private IEnumerable<SubFingerprintData> GetSubFingerprints(int totalCount, int countPerTrack)
+        {
+            var subFingerprints = new List<SubFingerprintData>();
+            int tracksCount = totalCount / countPerTrack;
+            for (int i = 0; i < totalCount; ++i)
+            {
+                int trackId = i % tracksCount;
+                var subFingerprint = new SubFingerprintData(new long[25], i, i * 1.48f, new ModelReference<ulong>((ulong)i), new ModelReference<int>(trackId));
+                subFingerprints.Add(subFingerprint);
+            }
+
+            return subFingerprints;
+        }
+    }
+}