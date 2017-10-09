9544361ffac2986406bdb7248152a9d690205466
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2017-10-09 10:14:42 +0300

    Making sure we use upgrade `MSBuild` tools to compile the libraries. Also removing `MixedPlatform` configuration option since we don't really need to build libraries against various platform versions.

--- build.cmd
+++ build.cmd
@@ -7,4 +7,5 @@ set config=%2
 if "%config%" == "" (
    set config=Release
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build\Build.proj /t:"%target%" /p:Configuration="%config%" /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+"%programfiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild" build\Build.proj /t:"%target%" /p:Configuration="%config%" /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
