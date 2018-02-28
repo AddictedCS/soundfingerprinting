88471e5158fc115c3c448fa40f68ce9eeadc31d2
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2018-02-28 11:58:59 +0200

    Bumping to v4.2.0-alpha

--- src/SoundFingerprinting/Properties/AssemblyInfo.cs
+++ src/SoundFingerprinting/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("CrompAudioFingerprinting")]
 [assembly: InternalsVisibleTo("CrompAudioFingerprintingMaster")]
 
-[assembly: AssemblyVersion("4.1.0")]
-[assembly: AssemblyInformationalVersion("4.1.0")]
+[assembly: AssemblyVersion("4.2.0")]
+[assembly: AssemblyInformationalVersion("4.2.0-alpha")]
