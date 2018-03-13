188ba029d64b852261ca315d85eadf2cb5bdad53
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2018-03-13 19:43:51 +0200

    Preparing stable release 5.0.0

--- src/SoundFingerprinting/Properties/AssemblyInfo.cs
+++ src/SoundFingerprinting/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("SoundFingerprinting.Storage")]
 
 [assembly: AssemblyVersion("5.0.0")]
-[assembly: AssemblyInformationalVersion("5.0.0-alpha")]
+[assembly: AssemblyInformationalVersion("5.0.0")]
