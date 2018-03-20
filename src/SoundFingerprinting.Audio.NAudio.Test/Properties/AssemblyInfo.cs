7245d4497059e1c3d40a56461b88ae4c9c602cf5
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2018-03-20 11:45:53 +0200

    Building NAudio.

--- src/SoundFingerprinting.Audio.NAudio.Test/Properties/AssemblyInfo.cs
+++ src/SoundFingerprinting.Audio.NAudio.Test/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("5a5c0ae3-f864-4c73-96ec-7f9c9a338c53")]
 [assembly: AssemblyVersion("5.1.0")]
-[assembly: AssemblyInformationalVersion("5.1.0-alpha")]
+[assembly: AssemblyInformationalVersion("5.1.0")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
