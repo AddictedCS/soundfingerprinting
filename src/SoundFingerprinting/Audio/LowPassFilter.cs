cd615a1613d522745f21bcd7ac789bf24067be06
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2018-04-29 12:14:20 +0300

    Trying to get rid of aliasing

--- src/SoundFingerprinting/Audio/LowPassFilter.cs
+++ src/SoundFingerprinting/Audio/LowPassFilter.cs
@@ -16,12 +16,20 @@ namespace SoundFingerprinting.Audio
          * All filters are interpolating around 32 points
          */
 
-        private static float[] lp_filter48 =
+        private static float[] lp_filter49to48Khz =
         {
-            -0.0012990f, -0.0019311f, -0.0029319f, -0.0041300f, -0.0049453f, -0.0044366f, -0.0014823f, 0.0049420f,
-            0.0154518f, 0.0299911f, 0.0476932f, 0.0669171f, 0.0854657f, 0.1009527f, 0.1112388f, 0.1148437f, 0.1112388f,
-            0.1009527f, 0.0854657f, 0.0669171f, 0.0476932f, 0.0299911f, 0.0154518f, 0.0049420f, -0.0014823f,
-            -0.0044366f, -0.0049453f, -0.0041300f, -0.0029319f, -0.0019311f, -0.0012990f
+            0.0016977f, 0.0020362f, 0.0028684f, -0.0042343f, 0.0061380f, -0.0085451f, 0.0113830f, -0.0145443f,
+            0.0178923f, -0.0212695f, 0.0245076f, -0.0274382f, 0.0299043f, -0.0317711f, 0.0329347f, 0.9666700f,
+            0.0329347f, -0.0317711f, 0.0299043f, -0.0274382f, 0.0245076f, -0.0212695f, 0.0178923f, -0.0145443f,
+            0.0113830f, -0.0085451f, 0.0061380f, -0.0042343f, 0.0028684f, -0.0020362f, 0.0016977f
+        };
+
+        private static float[] lp_filter49Khz =
+        {
+            -1.4702e-03f, -2.0164e-03f, -2.8880e-03f, -3.8559e-03f, -4.3190e-03f, -3.3748e-03f, 1.7805e-07f,
+            6.6945e-03f, 1.7190e-02f, 3.1341e-02f, 4.8275e-02f, 6.6441e-02f, 8.3815e-02f, 9.8234e-02f, 1.0777e-01f,
+            1.1111e-01f, 1.0777e-01f, 9.8234e-02f, 8.3815e-02f, 6.6441e-02f, 4.8275e-02f, 3.1341e-02f, 1.7190e-02f,
+            6.6945e-03f, 1.7805e-07f, -3.3748e-03f, -4.3190e-03f, -3.8559e-03f, -2.8880e-03f, -2.0164e-03f, -1.4702e-03f
         };
 
         /*Low pass from 44100 to 5512*/
@@ -60,7 +68,7 @@ namespace SoundFingerprinting.Audio
             switch (sourceSampleRate)
             {
                 case 48000:
-                    return Resample(samples, (int)(samples.Length / 8.7082728d) - 31, 8.7082728d, lp_filter48);
+                    return ResampleNonIntegerFactor(samples, 29, 9, lp_filter49Khz);
                 case 44100:
                     return Resample(samples, samples.Length / 8 - 31, 8, lp_filter44);
                 case 22050:
@@ -74,22 +82,50 @@ namespace SoundFingerprinting.Audio
             throw new ArgumentException($"Not supported sample rate {sourceSampleRate}");
         }
 
-        private float[] Resample(float[] samples, int newSamplesCount, double mult, float[] filter)
+        private float[] ResampleNonIntegerFactor(float[] samples, int p, int q, float[] filter)
+        {
+            int zeros = samples.Length / p; // number of zeros to add
+
+            float[] buffer = new float[samples.Length + zeros];
+
+            for (int i = 0; i < zeros; ++i)
+            {
+                Buffer.BlockCopy(samples, i * p * sizeof(float), buffer, i * (p + 1) * sizeof(float), p * sizeof(float));
+                var sum = Convolve(samples, (i + 1) * p - 16, lp_filter49to48Khz, lp_filter49to48Khz.Length);
+                buffer[(i + 1) * (p + 1) - 1] = sum;
+            }
+
+            return Resample(buffer, buffer.Length / q - 31, q, filter);
+        }
+
+        private static float Avg(float[] samples, int p, int i)
+        {
+            float sum = 0f;
+            for (int j = (i + 1) * p - 16; j < (i + 1) * p + 16 && j < samples.Length; ++j)
+            {
+                sum += samples[j];
+            }
+
+            return sum / 32;
+        }
+
+        private float[] Resample(float[] samples, int newSamplesCount, int mult, float[] filter)
         {
             float[] resampled = new float[newSamplesCount];
             for (int i = 0; i < newSamplesCount; i++)
             {
-                resampled[i] = Convolve(samples, (int)(mult * i), filter, filter.Length);
+                resampled[i] = Convolve(samples, mult * i, filter, filter.Length);
             }
+
             return resampled;
         }
 
-        private float Convolve(float[] start, int startIndex, float[] filter, int flen)
+        private float Convolve(float[] buffer, int begin, float[] filter, int flen)
         {
             float sum = 0;
-            for (int i = 0; i < flen; i++)
+            for (int index = 0; index < flen && begin + index < buffer.Length; ++index)
             {
-                sum += start[startIndex + i] * filter[i];
+                sum += buffer[begin + index] * filter[index];
             }
 
             return sum;
