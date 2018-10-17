46be6d02c374175239d27020e34ab233762352ae
Sergiu Ciumac <sciumac@nemo.ai>
2018-10-17 14:42:38 +0300

    Adding some verbosity.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ else
     TARGET=$1    
 fi
 
-dotnet test src/SoundFingerprinting.Tests/SoundFingerprinting.Tests.csproj -c $TARGET /p:TargetFrameworks=netcoreapp2.1
+dotnet test src/SoundFingerprinting.Tests/SoundFingerprinting.Tests.csproj -c $TARGET /p:TargetFrameworks=netcoreapp2.1 -v n
 
 if [ "$?" != "0" ]; then
 	echo Tests failed. Check logs for details.
