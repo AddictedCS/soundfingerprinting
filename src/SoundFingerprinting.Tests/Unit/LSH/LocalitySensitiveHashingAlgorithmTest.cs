843586f00a45659f817bdf68edf6bd0f68ced8b2
Sergiu Ciumac <sciumac@nemo.ai>
2019-01-17 10:38:58 +0200

    Uniformly at random fingerprints should not generate more than 0.5 percents of collisions.

--- src/SoundFingerprinting.Tests/Unit/LSH/LocalitySensitiveHashingAlgorithmTest.cs
+++ src/SoundFingerprinting.Tests/Unit/LSH/LocalitySensitiveHashingAlgorithmTest.cs
@@ -49,5 +49,35 @@
                 }
             }
         }
+
+        [Test]
+        public void DistributionOfHashesHasToBeUniform()
+        {
+            var lshAlgorithm = new LocalitySensitiveHashingAlgorithm(new MinHashService(new MaxEntropyPermutations()), new HashConverter());
+
+            var random = new Random();
+
+            var storage = new RAMStorage(25, new IntModelReferenceProvider());
+            
+            float one = 8192f / 5512;
+            var config = new DefaultHashingConfig { NumberOfLSHTables = 25, NumberOfMinHashesPerTable = 4, HashBuckets = 0 };
+            
+            var track = new ModelReference<int>(1);
+            int l = 100000;
+            for (int i = 0; i < l; ++i)
+            {
+                int[] trues = Enumerable.Range(0, 200).Select(entry => random.Next(0, 8191)).ToArray();
+                var schema = new TinyFingerprintSchema(8192).SetTrueAt(trues);
+                var hash = lshAlgorithm.Hash(new Fingerprint(schema, i * one, (uint)i), config, Enumerable.Empty<string>());
+                storage.AddHashedFingerprint(hash, track);
+            }
+
+            var distribution = storage.HashCountsPerTable;
+
+            foreach (var hashPerTable in distribution)
+            {
+                Assert.IsTrue((double) (l - hashPerTable) / l <= 0.005d, "Less than 0.5 percents of collisions across 100K hashes");
+            }
+        }
     }
 }
