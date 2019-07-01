e199ad54ea162d6a249ced6566ae02baf293ad7c
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2019-07-01 12:45:13 +0300

    Version bump to 7.0.0

--- src/SoundFingerprinting/Properties/AssemblyInfo.cs
+++ src/SoundFingerprinting/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("SoundFingerprinting.FFT.FFTW")]
 [assembly: InternalsVisibleTo("SoundFingerprinting.FFT.FFTW.Tests")]
 
-[assembly: AssemblyVersion("6.2.1")]
-[assembly: AssemblyInformationalVersion("6.2.1")]
+[assembly: AssemblyVersion("7.0.0")]
+[assembly: AssemblyInformationalVersion("7.0.0")]
