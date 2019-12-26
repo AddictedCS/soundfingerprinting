c663d6842d535d7986e08cc493f1fc941404f8ba
Viorel Ghelbert <5279394+gviorel@users.noreply.github.com>
2019-12-26 20:28:39 +0200

    Keep the original SSIM image.

--- src/SoundFingerprinting/Image/SSIM.cs
+++ src/SoundFingerprinting/Image/SSIM.cs
@@ -4,13 +4,16 @@ namespace SoundFingerprinting.Image
 
     public class SSIM
     {
-        public SSIM(byte[][] ssimImage, IEnumerable<Contour> contours)
+        public SSIM(byte[][] ssimImage, byte[][] thresholdInvertedImage, IEnumerable<Contour> contours)
         {
             SsimImage = ssimImage;
+            ThresholdInvertedImage = thresholdInvertedImage;
             Contours = contours;
         }
-        
+
         public byte[][] SsimImage { get; }
+
+        public byte[][] ThresholdInvertedImage { get; }
         
         public IEnumerable<Contour> Contours { get; }
     }
