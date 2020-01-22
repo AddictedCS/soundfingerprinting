beac2bac4be1017c470151082a6ffb7e829086ab
Sergiu Ciumac <sciumac@nemo.ai>
2020-01-22 13:24:03 +0200

    We don't need equatable interfaces here.

--- src/SoundFingerprinting/LCS/MaxAt.cs
+++ src/SoundFingerprinting/LCS/MaxAt.cs
@@ -1,9 +1,8 @@
 namespace SoundFingerprinting.LCS
 {
-    using System;
     using SoundFingerprinting.Query;
 
-    internal class MaxAt : IEquatable<MaxAt>
+    internal class MaxAt
     {
         public MaxAt(int length, MatchedWith matchedWith)
         {
@@ -14,28 +13,5 @@ namespace SoundFingerprinting.LCS
         public int Length { get; }
         
         public MatchedWith MatchedWith { get; }
-
-        public bool Equals(MaxAt other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return Length == other.Length && Equals(MatchedWith, other.MatchedWith);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((MaxAt) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return (Length * 397) ^ (MatchedWith != null ? MatchedWith.GetHashCode() : 0);
-            }
-        }
     }
 }
--- src/SoundFingerprinting/Query/MatchedWith.cs
+++ src/SoundFingerprinting/Query/MatchedWith.cs
@@ -1,8 +1,6 @@
 ﻿namespace SoundFingerprinting.Query
 {
-    using System;
-
-    public class MatchedWith : IEquatable<MatchedWith>
+    public class MatchedWith
     {
         public MatchedWith(uint querySequenceNumber, float queryMatchAt, uint trackSequenceNumber, float trackMatchAt, double score)
         {
@@ -22,33 +20,5 @@
         public float TrackMatchAt { get; }
 
         public double Score { get; }
-
-        public bool Equals(MatchedWith other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return QuerySequenceNumber == other.QuerySequenceNumber && QueryMatchAt.Equals(other.QueryMatchAt) && TrackSequenceNumber == other.TrackSequenceNumber && TrackMatchAt.Equals(other.TrackMatchAt) && Score.Equals(other.Score);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((MatchedWith) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int hashCode = (int) QuerySequenceNumber;
-                hashCode = (hashCode * 397) ^ QueryMatchAt.GetHashCode();
-                hashCode = (hashCode * 397) ^ (int) TrackSequenceNumber;
-                hashCode = (hashCode * 397) ^ TrackMatchAt.GetHashCode();
-                hashCode = (hashCode * 397) ^ Score.GetHashCode();
-                return hashCode;
-            }
-        }
     }
 }
