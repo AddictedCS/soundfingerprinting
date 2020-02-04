c0a4aa6777fa40c9aefdb4926a2040fec6f9461e
Viorel Ghelbert <5279394+gviorel@users.noreply.github.com>
2020-02-04 22:06:12 +0200

    Add more tests.

--- src/SoundFingerprinting.Tests/Unit/Query/ConfidenceCalculatorTest.cs
+++ src/SoundFingerprinting.Tests/Unit/Query/ConfidenceCalculatorTest.cs
@@ -131,6 +131,34 @@
          * Track: =====
          */
         [TestCase(0, 5, 5, 0, 5, ExpectedResult = 1)]
+        /* Query: ---=------
+         * Track: =---------
+         */
+        [TestCase(3, 1, 10, 0, 10, ExpectedResult = 0.14285714285714285)]
+        /* Query: ---==-----
+         * Track: ==--------
+         */
+        [TestCase(3, 2, 10, 0, 10, ExpectedResult = 0.2857142857142857)]
+        /* Query: ---===----
+         * Track: ===-------
+         */
+        [TestCase(3, 3, 10, 0, 10, ExpectedResult = 0.42857142857142855)]
+        /* Query: ---====---
+         * Track: ====------
+         */
+        [TestCase(3, 4, 10, 0, 10, ExpectedResult = 0.5714285714285714)]
+        /* Query: ---=====--
+         * Track: =====-----
+         */
+        [TestCase(3, 5, 10, 0, 10, ExpectedResult = 0.7142857142857143)]
+        /* Query: ---======-
+         * Track: ======----
+         */
+        [TestCase(3, 6, 10, 0, 10, ExpectedResult = 0.8571428571428571)]
+        /* Query: ---=======
+         * Track: =======---
+         */
+        [TestCase(3, 7, 10, 0, 10, ExpectedResult = 1)]
         public double QueryAndTrackOfEqualLength(double queryMatchStartsAt, double coverageLength, double queryLength,
             double trackMatchStartsAt, double trackLength)
         {
@@ -140,19 +168,19 @@
 
         /* Query: -----
          * Track: ----------
-          */
+         */
         [TestCase(0, 0, 5, 0, 10, ExpectedResult = 0)]
         /* Query: ==---
          * Track: ==--------
-          */
+         */
         [TestCase(0, 2, 5, 0, 10, ExpectedResult = 0.4)]
         /* Query: -==--
          * Track: ==--------
-          */
+         */
         [TestCase(1, 2, 5, 0, 10, ExpectedResult = 0.5)]
         /* Query: ---==
          * Track: ==--------
-          */
+         */
         [TestCase(3, 2, 5, 0, 10, ExpectedResult = 1)]
         /* Query: ==---
          * Track: ---==-----
