5863c7d55f0fa7f019bed1e507b58a04f16c0240
Sergiu Ciumac <sciumac@nemo.ai>
2020-12-14 21:37:26 +0200

    We can do it outside of the parallel foreach as pointed by David R.

--- src/SoundFingerprinting/Configuration/Frames/LogSpectrumNormalization.cs
+++ src/SoundFingerprinting/Configuration/Frames/LogSpectrumNormalization.cs
@@ -6,21 +6,20 @@ namespace SoundFingerprinting.Configuration.Frames
 
     public class LogSpectrumNormalization : IFrameNormalization
     {
+        private const int Domain = 255;
         public IEnumerable<Frame> Normalize(IEnumerable<Frame> frames)
         {
             return frames
                 .AsParallel()
                 .Select(frame =>
                 {
-
+                    float c = (float) (1 / System.Math.Log(1 + Domain));
                     float max = frame.ImageRowCols.Max(System.Math.Abs);
                     for (int i = 0; i < frame.ImageRowCols.Length; ++i)
                     {
                         float value = frame.ImageRowCols[i];
                         float scaled = System.Math.Min(value / max, 1);
-                        int domain = 255;
-                        float c = (float) (1 / System.Math.Log(1 + domain));
-                        frame.ImageRowCols[i] = (float) (c * System.Math.Log(1 + scaled * domain));
+                        frame.ImageRowCols[i] = (float) (c * System.Math.Log(1 + scaled * Domain));
                     }
 
                     return frame;
