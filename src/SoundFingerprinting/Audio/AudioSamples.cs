6376c54e5bb5faf8700407fbb0e6b36d764794e1
Sergiu Ciumac <sciumac@nemo.ai>
2021-03-17 12:56:14 +0200

    Adding audio samples offset to be able to offset the matches when overlaid queries are used (see RealtimeAudioSamplesAggregator).

--- src/SoundFingerprinting/Audio/AudioSamples.cs
+++ src/SoundFingerprinting/Audio/AudioSamples.cs
@@ -1,6 +1,7 @@
 ﻿namespace SoundFingerprinting.Audio
 {
     using System;
+    using SoundFingerprinting.Command;
 
     /// <summary>
     ///  Audio samples that can be used for fingerprinting
@@ -13,12 +14,27 @@
             // no op
         }
 
-        public AudioSamples(float[] samples, string origin, int sampleRate, DateTime relativeTo)
+       
+        public AudioSamples(float[] samples, string origin, int sampleRate, DateTime relativeTo) : this(samples, origin, sampleRate, relativeTo, 0d)
+        {
+            // no op
+        }
+        
+        /// <summary>
+        ///  Creates new instance of AudioSamples class.
+        /// </summary>
+        /// <param name="samples">Audio samples.</param>
+        /// <param name="origin">Source origin (i.e., filename, URI).</param>
+        /// <param name="sampleRate">Sample rate.</param>
+        /// <param name="relativeTo">Relative to a particular date time offset.</param>
+        /// <param name="offset">Offset of the captured audio samples.</param>
+        public AudioSamples(float[] samples, string origin, int sampleRate, DateTime relativeTo, double offset)
         {
             Samples = samples;
             Origin = origin;
             SampleRate = sampleRate;
             RelativeTo = relativeTo;
+            Offset = offset;
         }
 
         private AudioSamples()
@@ -47,8 +63,16 @@
         public DateTime RelativeTo { get; }
 
         /// <summary>
-        ///  Gets the duration of the audio samples
+        ///  Gets the duration in seconds of the audio samples
         /// </summary>
         public double Duration => (double)Samples.Length / SampleRate;
+
+        /// <summary>
+        ///  Gets of offset of the captured audio samples relative to the previous capture.
+        /// </summary>
+        /// <remarks>
+        ///  Used by <see cref="RealtimeQueryCommand"/>, 0 in normal request/reply scenarios.
+        /// </remarks>
+        public double Offset { get; }
     }
 }
