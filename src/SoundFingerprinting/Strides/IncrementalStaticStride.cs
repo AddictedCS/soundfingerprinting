e518fe26a11e7f96fe177d199b7e1741fa6d1c5e
Sergiu Ciumac <sciumac@nemo.ai>
2021-10-12 13:38:10 +0300

    More comments.

--- src/SoundFingerprinting/Strides/IncrementalStaticStride.cs
+++ src/SoundFingerprinting/Strides/IncrementalStaticStride.cs
@@ -1,12 +1,12 @@
 ﻿namespace SoundFingerprinting.Strides
 {
     /// <summary>
-    ///  Incremental static stride used in providing an exact step length (measured in number of audio samples) between 2 consecutive fingerprints
+    ///  Incremental static stride used in providing an exact step length (measured in number of audio samples) between 2 consecutive fingerprints.
     /// </summary>
     public class IncrementalStaticStride : IStride
     {
         /// <summary>
-        ///  Creates new instance of <see cref="IncrementalStaticStride"/>.
+        /// Initializes a new instance of the <see cref="IncrementalStaticStride"/> class.
         /// </summary>
         /// <param name="incrementBy">Size is samples to be used between two consecutive fingerprints.</param>
         /// <param name="firstStride">Size in samples of the first stride.</param>
@@ -15,7 +15,7 @@
             NextStride = incrementBy;
             FirstStride = firstStride;
         }
-        
+
         /// <inheritdoc cref="IStride.FirstStride"/>
         public int FirstStride { get; }
 
