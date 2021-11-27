55af8a75861b466453cc43d195c05f41e16d85de
Sergiu Ciumac <sciumac@nemo.ai>
2021-11-27 21:18:48 +0200

    Improving TrackInfo interface. Making sure it is IEquatable class since we already override Equals and GetHashCode where both Id and MediaType is considered.

--- src/SoundFingerprinting/Data/TrackInfo.cs
+++ src/SoundFingerprinting/Data/TrackInfo.cs
@@ -5,15 +5,32 @@
     using System.Linq;
     using ProtoBuf;
 
+    /// <summary>
+    ///  Class describing a track.
+    /// </summary>
     [Serializable]
     [ProtoContract]
-    public class TrackInfo
+    public class TrackInfo : IEquatable<TrackInfo>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrackInfo"/> class.
+        /// </summary>
+        /// <param name="id">Track unique identifier.</param>
+        /// <param name="title">Track title.</param>
+        /// <param name="artist">Track artist.</param>
         public TrackInfo(string id, string title, string artist) : this(id, title, artist, new Dictionary<string, string>())
         {
             // no op
         }
-        
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrackInfo"/> class.
+        /// </summary>
+        /// <param name="id">Track unique identifier.</param>
+        /// <param name="title">Track title.</param>
+        /// <param name="artist">Track artist.</param>
+        /// <param name="metaFields">Track meta fields.</param>
+        /// <param name="mediaType">Track media type (default MediaType.Audio).</param>
         public TrackInfo(string id, string title, string artist, IDictionary<string, string> metaFields, MediaType mediaType = MediaType.Audio)
         {
             Id = id;
@@ -30,20 +47,37 @@
             MediaType = MediaType.Audio;
         }
 
-        [ProtoMember(1)] public string Id { get; }
+        /// <summary>
+        ///  Gets tracks unique identifier.
+        /// </summary>
+        [ProtoMember(1)] 
+        public string Id { get; }
 
-        [ProtoMember(2)]
+        /// <summary>
+        ///  Gets track title.
+        /// </summary>
+        [ProtoMember(2)] 
         public string Title { get; }
 
-        [ProtoMember(3)]
+        /// <summary>
+        ///  Gets track artist.
+        /// </summary>
+        [ProtoMember(3)] 
         public string Artist { get; }
 
-        [ProtoMember(4)]
+        /// <summary>
+        ///  Gets track meta fields.
+        /// </summary>
+        [ProtoMember(4)] 
         public IDictionary<string, string> MetaFields { get; }
 
-        [ProtoMember(5)]
+        /// <summary>
+        ///  Gets track media type.
+        /// </summary>
+        [ProtoMember(5)] 
         public MediaType MediaType { get; }
 
+        /// <inheritdoc cref="object.ToString"/>
         public override string ToString()
         {
             return $"TrackInfo Id: {Id}, " +
@@ -53,20 +87,35 @@
                    $"MetaFields {string.Join(",", MetaFields.Select(pair => $"{pair.Key}:{pair.Value}"))}";
         }
 
+        /// <inheritdoc cref="object.GetHashCode"/>
         public override int GetHashCode()
         {
-            return Id.GetHashCode();
+            unchecked
+            {
+                return (Id.GetHashCode() * 397) ^ (int)MediaType;
+            }
         }
 
+        /// <inheritdoc cref="object.Equals(object)"/>
         public override bool Equals(object obj)
         {
-            if (obj == null)
-                return false;
+            return Equals(obj as TrackInfo);
+        }
 
-            if (!(obj is TrackInfo trackInfo))
+        /// <inheritdoc cref="object.Equals(object)"/>
+        public bool Equals(TrackInfo? other)
+        {
+            if (ReferenceEquals(null, other))
+            {
                 return false;
+            }
 
-            return Id.Equals(trackInfo.Id) && MediaType.Equals(trackInfo.MediaType);
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return Id == other.Id && MediaType == other.MediaType;
         }
     }
 }
