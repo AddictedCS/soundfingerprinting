02913f749fd3fe47431f53e159d5f57f1794d6d1
Sergiu Ciumac <sciumac@nemo.ai>
2021-11-27 21:18:01 +0200

    Adding comments to QueryMatch class.

--- src/SoundFingerprinting/Data/QueryMatch.cs
+++ src/SoundFingerprinting/Data/QueryMatch.cs
@@ -1,15 +1,22 @@
-// ReSharper disable UnusedMember.Local
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable UnusedAutoPropertyAccessor.Global
 namespace SoundFingerprinting.Data
 {
     using System;
     using ProtoBuf;
     using SoundFingerprinting.LCS;
 
+    /// <summary>
+    ///  Class that describes a successful query match.
+    /// </summary>
     [ProtoContract]
     public class QueryMatch
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QueryMatch"/> class.
+        /// </summary>
+        /// <param name="queryMatchId">Unique query match identifier.</param>
+        /// <param name="track">Track corresponding to the query match.</param>
+        /// <param name="coverage">An instance of the <see cref="Coverage"/> class, describing the match.</param>
+        /// <param name="matchedAt">Matched at (calculated relative to <see cref="Hashes.RelativeTo"/> property).</param>
         public QueryMatch(string queryMatchId, TrackInfo track, Coverage coverage, DateTime matchedAt)
         {
             QueryMatchId = queryMatchId;
@@ -17,22 +24,34 @@ namespace SoundFingerprinting.Data
             Coverage = coverage;
             MatchedAt = matchedAt;
         }
-        
+
         private QueryMatch()
         {
             // left for proto-buf
         }
-        
-        [ProtoMember(1)]
+
+        /// <summary>
+        ///  Gets an instance of the <see cref="TrackInfo"/> that matched the query.
+        /// </summary>
+        [ProtoMember(1)] 
         public TrackInfo Track { get; }
-        
-        [ProtoMember(2)]
+
+        /// <summary>
+        ///  Gets coverage information about the match.
+        /// </summary>
+        [ProtoMember(2)] 
         public Coverage Coverage { get; }
 
-        [ProtoMember(3)]
+        /// <summary>
+        ///  Gets matched at reference (calculated relative to <see cref="Hashes.RelativeTo"/> property).
+        /// </summary>
+        [ProtoMember(3)] 
         public DateTime MatchedAt { get; }
 
-        [ProtoMember(4)]
+        /// <summary>
+        ///  Gets query matches unique identifier.
+        /// </summary>
+        [ProtoMember(4)] 
         public string QueryMatchId { get; }
     }
 }
