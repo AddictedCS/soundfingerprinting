0dc9bdaa3b977851e3b34a315b5aa948c739c031
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2021-12-23 18:34:02 +0200

    Version bump

--- src/SoundFingerprinting.Audio.NAudio.Test/Properties/AssemblyInfo.cs
+++ src/SoundFingerprinting.Audio.NAudio.Test/Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("5a5c0ae3-f864-4c73-96ec-7f9c9a338c53")]
-[assembly: AssemblyVersion("8.0.0.103")]
-[assembly: AssemblyInformationalVersion("8.0.0.103")]
+[assembly: AssemblyVersion("8.0.0.105")]
+[assembly: AssemblyInformationalVersion("8.0.0.105")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
