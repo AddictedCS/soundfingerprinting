a3ff102ff40ee2a134b00c473de4dea3c0e4d4b2
Sergiu Ciumac <sciumac@nemo.ai>
2022-02-23 09:56:55 +0200

    Improving usages.

--- src/SoundFingerprinting/Command/QueryCommand.cs
+++ src/SoundFingerprinting/Command/QueryCommand.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Linq;
-    using System.Runtime.InteropServices.ComTypes;
     using System.Threading.Tasks;
     using Microsoft.Extensions.Logging;
     using SoundFingerprinting.Audio;
