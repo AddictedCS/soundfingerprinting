9a2f21519fa7a0a11857dddc82d1b1729ccf341a
Sergiu Ciumac <sciumac@nemo.ai>
2022-04-17 10:30:57 +0300

    Version bump.

--- src/SoundFingerprinting/Properties/AssemblyInfo.cs
+++ src/SoundFingerprinting/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("SoundFingerprinting.FFT.FFTW")]
 [assembly: InternalsVisibleTo("SoundFingerprinting.FFT.FFTW.Tests")]
 
-[assembly: AssemblyVersion("8.7.0.100")]
-[assembly: AssemblyInformationalVersion("8.7.0.100")]
+[assembly: AssemblyVersion("8.8.0.100")]
+[assembly: AssemblyInformationalVersion("8.8.0.100")]
