29d5371db4572fa01c1ea0e9ae879095b8d37d70
Sergiu Ciumac <sciumac@nemo.ai>
2022-11-22 10:57:59 +0200

    Adding more comments to publicly visible types.

--- src/SoundFingerprinting/DAO/ModelReference.cs
+++ src/SoundFingerprinting/DAO/ModelReference.cs
@@ -3,10 +3,18 @@
     using System;
     using ProtoBuf;
 
+    /// <summary>
+    ///  Storage model reference.
+    /// </summary>
+    /// <typeparam name="T">Type parameter.</typeparam>
     [Serializable]
     [ProtoContract]
     public class ModelReference<T> : IModelReference
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModelReference{T}"/> class.
+        /// </summary>
+        /// <param name="id">Identifier.</param>
         public ModelReference(T id)
         {
             Id = id;
@@ -17,11 +25,23 @@
             // left for proto-buf
         }
 
+        /// <summary>
+        ///  Gets a null model reference.
+        /// </summary>
         public static ModelReference<T> Null { get; } = new ModelReference<T>(default);
 
+        /// <summary>
+        ///  Gets underlying identifier for the model reference.
+        /// </summary>
         [ProtoMember(1)]
         public T Id { get; }
         
+        /// <summary>
+        ///  Casts underlying <see cref="Id"/> to requested type.
+        /// </summary>
+        /// <typeparam name="TOut">Type parameter.</typeparam>
+        /// <returns><see cref="Id"/> casted to request type parameter.</returns>
+        /// <exception cref="InvalidCastException">Throws invalid cast exception.</exception>
         public TOut Get<TOut>()
         {
             if (Id is TOut get)
@@ -32,21 +52,24 @@
             throw new InvalidCastException($"{typeof(T)} cannot be converted to {typeof(TOut)}");
         }
 
+        /// <inheritdoc cref="object.Equals(object)"/>
         public override bool Equals(object obj)
         {
-            if (!(obj is ModelReference<T> @object))
+            if (obj is not ModelReference<T> @object)
             {
                 return false;
             }
 
-            return Id.Equals(@object.Id);
+            return Id.Equals(@object.Id); 
         }
 
+        /// <inheritdoc cref="object.GetHashCode"/>
         public override int GetHashCode()
         {
             return Id.GetHashCode();
         }
 
+        /// <inheritdoc cref="object.ToString"/>
         public override string ToString()
         {
             return $"ModelReference {nameof(Id)}: {Id}";
