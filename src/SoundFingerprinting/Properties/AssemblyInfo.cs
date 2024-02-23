300acaec1b8240f530fac19ae816f80f124ec259
Sergiu Ciumac <sergiu@emysound.com>
2024-02-23 10:45:43 +0200

    Release a beta version for testing new Query Candidates.

--- src/SoundFingerprinting/Properties/AssemblyInfo.cs
+++ src/SoundFingerprinting/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("SoundFingerprinting.FFT.FFTW")]
 [assembly: InternalsVisibleTo("SoundFingerprinting.FFT.FFTW.Tests")]
 
-[assembly: AssemblyVersion("9.3.0.100")]
-[assembly: AssemblyInformationalVersion("9.3.0.100")]
+[assembly: AssemblyVersion("9.4.0.100")]
+[assembly: AssemblyInformationalVersion("9.4.0.100")]
