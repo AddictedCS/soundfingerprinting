af63396d0e441cb487e73c4f2e98d8642a6ca809
Sergiu Ciumac <sergiu@emysound.com>
2026-01-10 14:03:14 +0200

    We can precompute norm reciprocal and use it during wavelet decomposition.

--- src/SoundFingerprinting/Wavelets/StandardHaarWaveletDecomposition.cs
+++ src/SoundFingerprinting/Wavelets/StandardHaarWaveletDecomposition.cs
@@ -1,4 +1,4 @@
-﻿namespace SoundFingerprinting.Wavelets
+namespace SoundFingerprinting.Wavelets
 {
     using System;
 
@@ -17,33 +17,35 @@
             DecomposeImage(image, rows, cols, waveletNorm);
         }
 
-        private void DecompositionArray(float[] array, float[] temp, double waveletNorm)
+        private void DecompositionArray(float[] array, float[] temp, float normReciprocal)
         {
             int h = array.Length;
             while (h > 1)
             {
-                DecompositionStep(array, h, 0, temp, waveletNorm);
+                DecompositionStep(array, h, 0, temp, normReciprocal);
                 h /= 2;
             }
         }
 
-        private void DecompositionRow(float[] array, int row, int cols, float[] temp, double waveletNorm)
+        private void DecompositionRow(float[] array, int row, int cols, float[] temp, float normReciprocal)
         {
             int h = cols;
             while (h > 1)
             {
-                DecompositionStep(array, h, row * cols, temp, waveletNorm);
+                DecompositionStep(array, h, row * cols, temp, normReciprocal);
                 h /= 2;
             }
         }
 
-        private void DecompositionStep(float[] array, int h, int prefix, float[] temp, double waveletNorm)
+        private void DecompositionStep(float[] array, int h, int prefix, float[] temp, float normReciprocal)
         {
             h /= 2;
             for (int i = 0, j = 0; i < h; ++i, j = 2 * i)
             {
-                temp[i] = (float)((array[prefix + j] + array[prefix + j + 1]) / waveletNorm);
-                temp[i + h] = (float)((array[prefix + j] - array[prefix + j + 1]) / waveletNorm);
+                float left = array[prefix + j];
+                float right = array[prefix + j + 1];
+                temp[i] = (left + right) * normReciprocal;
+                temp[i + h] = (left - right) * normReciprocal;
             }
 
             Buffer.BlockCopy(temp, 0, array, prefix * sizeof(float), sizeof(float) * (h * 2));
@@ -53,6 +55,7 @@
         {
             float[] temp = new float[rows > cols ? rows : cols];
             float[] column = new float[rows]; /*Length of each column is equal to number of rows*/
+            float normReciprocal = (float)(1.0 / waveletNorm);
 
             // The order of decomposition is reversed because the image is 128x32 but we consider it reversed 32x128
             for (int col = 0; col < cols /*32*/; col++)
@@ -62,7 +65,7 @@
                     column[colIndex] = image[col + (colIndex * cols)]; /*Copying Column vector*/
                 }
 
-                DecompositionArray(column, temp, waveletNorm); /*Decomposition of each row*/
+                DecompositionArray(column, temp, normReciprocal); /*Decomposition of each row*/
                 for (int colIndex = 0; colIndex < rows; colIndex++)
                 {
                     image[col  + (cols * colIndex)] = column[colIndex];
@@ -71,7 +74,7 @@
 
             for (int row = 0; row < rows /*128*/; row++)
             {
-                DecompositionRow(image, row, cols, temp, waveletNorm); /*Decomposition of each row*/
+                DecompositionRow(image, row, cols, temp, normReciprocal); /*Decomposition of each row*/
             }
         }
     }
