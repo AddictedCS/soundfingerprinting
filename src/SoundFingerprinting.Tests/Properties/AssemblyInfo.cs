276236b62bd51058cf975c48fcbc4f99f56c11fe
Sergiu Ciumac <sergiu@emysound.com>
2026-05-13 11:22:06 +0300

    Upgrading dependencies.

--- src/SoundFingerprinting.Tests/Properties/AssemblyInfo.cs
+++ src/SoundFingerprinting.Tests/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4cac962e-ebc5-4006-a1e0-7ffb3e2483c2")]
-[assembly: AssemblyVersion("14.1.0.100")]
-[assembly: AssemblyInformationalVersion("14.1.0.100")]
+[assembly: AssemblyVersion("15.0.0.100")]
+[assembly: AssemblyInformationalVersion("15.0.0.100")]
