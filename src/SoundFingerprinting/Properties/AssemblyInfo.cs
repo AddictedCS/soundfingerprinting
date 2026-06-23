8588fca94d4c5d5cd035ef5cfb2e3de7647b9c57
Sergiu Ciumac <sergiu@emysound.com>
2026-06-23 10:56:12 +0300

    Version bump to v15.6.0

--- src/SoundFingerprinting/Properties/AssemblyInfo.cs
+++ src/SoundFingerprinting/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("SoundFingerprinting.FFT.FFTW")]
 [assembly: InternalsVisibleTo("SoundFingerprinting.FFT.FFTW.Tests")]
 
-[assembly: AssemblyVersion("15.5.1.100")]
-[assembly: AssemblyInformationalVersion("15.5.1.100")]
+[assembly: AssemblyVersion("15.6.0.100")]
+[assembly: AssemblyInformationalVersion("15.6.0.100")]
